ff478f910f6f47d28e7c99a76a02e7331b0c611f
Filipe Guimarães <fmtfg99@gmail.com>
2020-08-21 15:18:43 +0100

    20200821

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,4 +7,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar main -c $HOME/.config/polybar/config.ini &
+polybar top -r -c ~/.config/polybar/polybar.conf &
